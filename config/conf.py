8af105a490bc72088223a50b2674c06c4534e9f2
MYMEILE <529266369@qq.com>
2024-12-07 16:50:19 +0800

    add

--- config/conf.py
+++ config/conf.py
@@ -19,7 +19,10 @@ class Settings(BaseSettings):
     MAX_RETRIES: int = 5
     DELAY: int = 5
 
-    DATABASE_URI: str = "mysql+aiomysql://root:mm123123@127.0.0.1:3306/mbti"
+    # 测试环境
+    # DATABASE_URI: str = "mysql+aiomysql://root:mm123123@127.0.0.1:3306/mbti"
+    # 正式环境
+    DATABASE_URI: str = "mysql+aiomysql://rcb:cryptoBricks123@cb-rds.cw5tnk9dgstt.us-west-2.rds.amazonaws.com:3306/mbti"
 
 
 settings = Settings()
